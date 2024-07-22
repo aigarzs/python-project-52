9954ad146a6538f9cad4c26840a6c06c5173ea0a
Aigars Sinevics <aigarzs@gmail.com>
2024-07-22 15:11:45 +0300

    Created View Hello World

---
+++ Makefile
@@ -0,0 +1,10 @@
+start:
+	poetry run python manage.py runserver
+
+
+dev:
+	poetry run python manage.py runserver
+
+
+django-shell:
+	poetry run python manage.py shell
---
+++ task_manager/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.views import View
+
+
+class HomeView(View):
+
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("Hello World!")
+
